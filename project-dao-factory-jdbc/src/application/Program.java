497e317c77334cbe86cab9bae9eddb459a272f71
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-02-24 13:28:55 -0300

    Criação da classe entidade - Seller

--- project-dao-factory-jdbc/src/application/Program.java
+++ project-dao-factory-jdbc/src/application/Program.java
@@ -1,13 +1,28 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
 
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now() , 3000.00, obj);
+		System.out.println(seller);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- project-dao-factory-jdbc/src/model/entities/Department.java
+++ project-dao-factory-jdbc/src/model/entities/Department.java
@@ -5,7 +5,8 @@ import java.util.Objects;
 
 public class Department implements Serializable{
 	
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
+	
 //Atributos da classe:
 	private Integer id;
 	private String name;
