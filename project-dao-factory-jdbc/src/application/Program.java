3edeae9ac2bdcb05c30c5cbd5319471cc7d361ff
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-03-01 11:41:01 -0300

    Método findByDepartment - Implementado

--- project-dao-factory-jdbc/src/application/Program.java
+++ project-dao-factory-jdbc/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,8 +18,12 @@ public class Program {
 		
 		System.out.println(seller);
 		
-		
-		
+		System.out.println("\n==== TEST 2: seller method findByDepartment ====");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 		
 		
 		
