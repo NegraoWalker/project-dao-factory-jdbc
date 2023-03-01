e154a69f060ed3b149d81d256543db63d755762e
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-03-01 14:45:43 -0300

    Métodos findAll e insert - Implementados

--- project-dao-factory-jdbc/src/application/Program.java
+++ project-dao-factory-jdbc/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DAOFactory;
@@ -13,18 +14,29 @@ public class Program {
 
 		SellerDAO sellerDao = DAOFactory.createSellerDAO();
 		
-		System.out.println("==== TEST 1: seller method findById ====");
-		Seller seller = sellerDao.findById(3);
+//		System.out.println("==== TEST 1: seller method findById ====");
+//		Seller seller = sellerDao.findById(3);
+//		
+//		System.out.println(seller);
+//		
+//		System.out.println("\n==== TEST 2: seller method findByDepartment ====");
+//		Department dep = new Department(2, null);
+//		List<Seller> list = sellerDao.findByDepartment(dep);
+//		for (Seller obj : list) {
+//			System.out.println(obj);
+//		}
+//		
+//		System.out.println("\n==== TEST 3: seller method findByAll ====");
+//		List<Seller> list = sellerDao.findAll();
+//		for (Seller obj : list) {
+//			System.out.println(obj);
+//		}
 		
-		System.out.println(seller);
-		
-		System.out.println("\n==== TEST 2: seller method findByDepartment ====");
+		System.out.println("\n==== TEST 4: seller method insert ====");
 		Department dep = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(dep);
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
-		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		
 	}
