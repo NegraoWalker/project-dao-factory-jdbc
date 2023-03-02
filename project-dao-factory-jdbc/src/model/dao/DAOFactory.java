9727fa2dc7529d71f6c817ad3b0ca8c0c290d0ef
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-03-02 15:05:52 -0300

    DepartmentDAOJDBC e test - Implementados

--- project-dao-factory-jdbc/src/model/dao/DAOFactory.java
+++ project-dao-factory-jdbc/src/model/dao/DAOFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.implementation.DepartmentDAOJDBC;
 import model.dao.implementation.SellerDAOJDBC;
 
 public class DAOFactory {
@@ -8,4 +9,11 @@ public class DAOFactory {
 	public static SellerDAO createSellerDAO() { //Método de fábrica que retorna um objeto da interface instanciando o objeto da classe que implementa a mesma.
 		return  new SellerDAOJDBC(DB.getConnection()); 
 	}
+	
+	public static DepartmentDAO createDepartmentDao() {
+		return new DepartmentDAOJDBC(DB.getConnection());
+	}
+	
+	
+	
 }
